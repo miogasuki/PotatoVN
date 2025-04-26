1890c7562d7043de0738e1091133d436b0491229
goldenpotato137 <goldenpotato137@gmail.com>
2025-04-27 00:04:34 +0800

    chore(server): extend presigned URL expiry to 7 days

--- GalgameManager.Server/Services/OssService.cs
+++ GalgameManager.Server/Services/OssService.cs
@@ -60,7 +60,7 @@ public class OssService(
             return await client.PresignedGetObjectAsync(new PresignedGetObjectArgs()
                 .WithBucket(BucketName)
                 .WithObject(GetFullKey(userId, objectFullName))
-                .WithExpiry(5 * 365 * 60 * 60)); // 5 years
+                .WithExpiry(604800)); // 7 days
         }
         catch (Exception e)
         {

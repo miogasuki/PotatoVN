0f92c506468c93f775d6c52e89e3ef05f855e2e8
goldenpotato137 <goldenpotato137@gmail.com>
2025-09-19 11:16:40 +0800

    fix(app): 修复一个与游戏源相关的多线程竞态bug

--- GalgameManager/Helpers/SourceServiceFactory.cs
+++ GalgameManager/Helpers/SourceServiceFactory.cs
@@ -7,20 +7,24 @@ namespace GalgameManager.Helpers;
 public static class SourceServiceFactory
 {
     private static readonly Dictionary<GalgameSourceType, IGalgameSourceService> SourceServices = new();
+    private static readonly object Lock = new();
 
     public static IGalgameSourceService GetSourceService(GalgameSourceType type)
     {
-        if (SourceServices.TryGetValue(type, out IGalgameSourceService? value)) return value;
-        value = type switch
+        lock (Lock)
         {
-            GalgameSourceType.LocalFolder => App.GetService<LocalFolderSourceService>(),
-            GalgameSourceType.UnKnown => throw new ArgumentException("UnKnow source"),
-            GalgameSourceType.LocalZip => throw new NotImplementedException(),
-            GalgameSourceType.Virtual => App.GetService<VirtualSourceService>(),
-            GalgameSourceType.Steam => App.GetService<SteamSourceService>(),
-            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
-        };
-        SourceServices[type] = value;
-        return value;
+            if (SourceServices.TryGetValue(type, out IGalgameSourceService? value)) return value;
+            value = type switch
+            {
+                GalgameSourceType.LocalFolder => App.GetService<LocalFolderSourceService>(),
+                GalgameSourceType.UnKnown => throw new ArgumentException("UnKnow source"),
+                GalgameSourceType.LocalZip => throw new NotImplementedException(),
+                GalgameSourceType.Virtual => App.GetService<VirtualSourceService>(),
+                GalgameSourceType.Steam => App.GetService<SteamSourceService>(),
+                _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+            };
+            SourceServices[type] = value;
+            return value;
+        }
     }
 }

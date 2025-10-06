68b13882c8844b4800fcb51d96e278cc624d30b1
goldenpotato137 <goldenpotato137@gmail.com>
2025-10-06 21:46:31 +0800

    feat(app): 插件系统 (7) * 插件注入账户界面UI接口

---
+++ GalgameManager.WinApp.Base/Contracts/PluginUi/IPluginAccount.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace GalgameManager.WinApp.Base.Contracts.PluginUi;
+
+/// <summary>
+/// 如果这个插件需要在账户界面显示自己的账户项，请实现这个接口
+/// </summary>
+public interface IPluginAccount
+{
+    public FrameworkElement CreateAccountUi();
+}

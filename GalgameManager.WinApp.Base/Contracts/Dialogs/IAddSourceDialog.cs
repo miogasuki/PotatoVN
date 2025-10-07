1c02175b8c5f0bbccccaf24b3959331c1eb22663
goldenpotato137 <goldenpotato137@gmail.com>
2025-10-08 00:13:48 +0800

    feat(app): 插件系统 (12) * 支持插件自定义新的游戏库类型(part1) 基础接口

---
+++ GalgameManager.WinApp.Base/Contracts/Dialogs/IAddSourceDialog.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace GalgameManager.WinApp.Base.Contracts.Dialogs;
+
+/// <summary>
+/// 这个接口会暴露插件需要控制的添加数据源对话框的数据 <br/>
+/// </summary>
+public interface IAddSourceDialog
+{
+    /// <summary>
+    /// 要添加的源的路径，当它不为空字符串时确认按钮才可用
+    /// </summary>
+    public string Path { get; set; }
+
+    /// <summary>
+    /// 如果message为empty，则关闭infobar显示
+    /// </summary>
+    /// <param name="severity"></param>
+    /// <param name="message"></param>
+    public void DisplayMsg(InfoBarSeverity severity, string message);
+}

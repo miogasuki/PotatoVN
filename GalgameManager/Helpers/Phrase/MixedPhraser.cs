ad700d6addc4bfd8244022e426e4eb9da81feace
goldenpotato137 <goldenpotato137@gmail.com>
2026-07-18 21:12:41 +0800

    chore(app): 默认关闭混合源的bangumi搜刮器，避免超时影响搜刮体验

--- GalgameManager/Helpers/Phrase/MixedPhraser.cs
+++ GalgameManager/Helpers/Phrase/MixedPhraser.cs
@@ -128,7 +128,7 @@ public class MixedPhraser : IGalInfoPhraser, IGalCharacterPhraser, IGalStaffPars
                     phraserTasks[rssType] = null;
             }
         }
-        
+
         Dictionary<RssType, Galgame> metas = new();
         Galgame result = new();
         foreach (var (rssType, task) in phraserTasks)
@@ -403,7 +403,7 @@ public class MixedPhraserOrder
     // 版本号，每次添加新搜刮器/添加新字段的时候都应该把这个数字+1，以便galgameCollectionService能够更新配置中已有的顺序配置
     // 更新配置不需要手动编写，已经在GalgameCollectionService中使用反射实现，会自动添加新的默认配置
     public const int Version = 14;
-    
+
     // 为什么使用ObservableCollection：为了能够在MixedPhraserOrderDialog中使顺序能够drag&drop
     // 所有变量都应该命名为：{字段名}Order，此处字段名应该与Galgame中对应的字段名一致（为了让GetValue中的反射能够找到对应的字段）
     public ObservableCollection<RssType> NameOrder { get; set; } = new();
@@ -470,7 +470,7 @@ public class MixedPhraserData : IGalInfoPhraserData
 
 public class MixedPhraserEnabled
 {
-    public bool BangumiEnabled { get; set; } = true;
+    public bool BangumiEnabled { get; set; } = false;
     public bool VndbEnabled { get; set; } = true;
     public bool YmgalEnabled { get; set; } = true;
     public bool SteamEnabled { get; set; } = true;
